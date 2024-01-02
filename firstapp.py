c9362d942e4b517e160b870f02fa9760b97c0dbb
omarA381 <110635840+omarA381@users.noreply.github.com>
2024-01-02 11:30:36 +0200

    firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlip import pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression,Lasso,Ridge
 from sklearn.svm import SVC
